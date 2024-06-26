ee3f808099d11ccc6ae1c85a8a51a3cd33b1a3ed
andreashl09 <a9279352@gmail.com>
2024-06-26 16:26:11 +0200

    add code comments

--- Aufgaben/Aufgaben_DAY_3/Array/ArrayReihenfolge.java
+++ Aufgaben/Aufgaben_DAY_3/Array/ArrayReihenfolge.java
@@ -2,18 +2,28 @@ public class ArrayReihenfolge {
 
     public static void main(String args[]) {
 
+        // Initialisierung des Arrays mit den Zahlen 1 bis 9
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        // Initialisierung des Zählers, der auf das letzte Element im Array zeigt
         int counter = arr.length - 1;
 
+        // Schleife läuft, solange der Zähler nicht negativ ist
         while (0 <= counter) {
 
+            // Überprüfung, ob der Zähler auf das erste Element im Array zeigt
             if (counter == 0) {
+                // Wenn ja, drucke das Element ohne ein Komma am Ende
                 System.out.print(arr[counter]);
             } else {
+                // Wenn nicht, drucke das Element gefolgt von einem Komma und einem Leerzeichen
                 System.out.print(arr[counter] + ", ");
             }
-            
+
+            // Verringere den Zähler um eins für das nächste Element in der nächsten
+            // Iteration
             counter--;
         }
+
     }
 }
--- Aufgaben/Aufgaben_DAY_3/Array/MaxMinArray.java
+++ Aufgaben/Aufgaben_DAY_3/Array/MaxMinArray.java
@@ -4,26 +4,29 @@ public class MaxMinArray {
 
         int arr[] = { 3, 44, 87, 2, 4, 99, 112, -1, 9, 14, 89, 21 };
 
-        int min = 0, max = 0;
-
-        // Array durchsuchen und die kleinste und größte Zahl im Terminal ausgeben
-
-        for (int i = 0; i < arr.length; i++) {
+        // Initialisierung der Variablen für die kleinste und größte Zahl im Array
+        int min = arr[0]; // Annahme: Das erste Element ist vorläufig das Minimum
+        int max = arr[0]; // Annahme: Das erste Element ist vorläufig das Maximum
+        
+        // Array durchsuchen und die kleinste und größte Zahl finden
+        for (int i = 1; i < arr.length; i++) {
             int arrayNumber = arr[i];
-
-            // wenn min größer ist als arrayNumber dann akkumuliere min
-            if (min > arrayNumber) {
+        
+            // Wenn arrayNumber kleiner ist als das aktuelle Minimum, aktualisiere min
+            if (arrayNumber < min) {
                 min = arrayNumber;
-                // falls wenn max kleiner ist als arrayNumber dann akkumiliere max
-            } else if (max < arrayNumber) {
+            }
+        
+            // Wenn arrayNumber größer ist als das aktuelle Maximum, aktualisiere max
+            if (arrayNumber > max) {
                 max = arrayNumber;
             }
-
         }
-        // Ausgabe der Variablen
         
-        System.out.println(max);
-        System.out.println(min);
+        // Ausgabe der Ergebnisse
+        System.out.println("Der höchste Wert ist: " + max);
+        System.out.println("Der kleinste Wert ist: " + min);
+        
 
     }
 }
