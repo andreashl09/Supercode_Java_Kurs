f6ee6f0912176dba64c8088745a1e3fbefb184c4
andreashl09 <a9279352@gmail.com>
2024-06-26 16:45:19 +0200

    rename foleder and add code comments

--- Aufgaben/Aufgaben_DAY_3/Zahlenreihe/ZahlenreiheOutput.java
+++ Aufgaben/Aufgaben_DAY_3/Zahlenreihe/ZahlenreiheOutput.java
@@ -4,45 +4,43 @@ public class ZahlenreiheOutput {
 
     public static void main(String[] args) {
 
-        // Summe aller Zahlen zwichen 1 - 100
-        int sum = 0;
-        int max = 100;
+        // Summe aller Zahlen zwischen 1 und 100
+        int sum = 0; // Variable zur Speicherung der Summe initialisieren
+        int max = 100; // Maximale Zahl für die Summenberechnung festlegen
 
+        // Schleife zur Berechnung der Summe aller Zahlen von 1 bis max
         for (int i = 0; i <= max; i++) {
-            sum += i;
-
+            sum += i; // Addiere die aktuelle Zahl i zur Summe hinzu
         }
-        System.out.println("Summe aller Zahlen: " + sum);
+        System.out.println("Summe aller Zahlen: " + sum); // Ausgabe der berechneten Summe
 
-        sum = 0;
+        sum = 0; // Summe zurücksetzen für die nächste Berechnung
 
-        // Summe aller geraden Zahlen zwischen 1 - 100
-
-        int i = 0;
+        // Summe aller geraden Zahlen zwischen 1 und 100
+        int i = 0; // Zählervariable initialisieren
         while (i <= max) {
-            boolean isStraightNumber = i % 2 == 0;
-            if (isStraightNumber) {
-                sum += i;
+            boolean isEvenNumber = i % 2 == 0; // Überprüfen, ob i eine gerade Zahl ist
+            if (isEvenNumber) {
+                sum += i; // Wenn i gerade ist, zur Summe hinzufügen
             }
-            i++;
+            i++; // Inkrementiere den Zähler
         }
+        System.out.println("Summe aller geraden Zahlen: " + sum); // Ausgabe der berechneten Summe der geraden Zahlen
 
-        System.out.println("Summe aller geraden Zahlen: " + sum);
-
-        // Summe aller ungeraden Zahlen zwischen 1 - 100
-        i = 0;
-        sum = 0;
+        // Summe aller ungeraden Zahlen zwischen 1 und 100
+        i = 0; // Zähler zurücksetzen für die nächste Berechnung
+        sum = 0; // Summe zurücksetzen für die Berechnung der ungeraden Zahlen
 
         do {
-            boolean isUnevenNumber = i % 2 == 1;
-            if (isUnevenNumber) {
-                sum += i;
+            boolean isOddNumber = i % 2 != 0; // Überprüfen, ob i eine ungerade Zahl ist
+            if (isOddNumber) {
+                sum += i; // Wenn i ungerade ist, zur Summe hinzufügen
             }
-
-            i++;
+            i++; // Inkrementiere den Zähler
         } while (i <= max);
 
-        System.out.println("Summe aller ungeraden Zahlen: " + sum);
+        System.out.println("Summe aller ungeraden Zahlen: " + sum); // Ausgabe der berechneten Summe der ungeraden
+                                                                    // Zahlen
     }
 
 }
