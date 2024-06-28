6b0543ff19f5df4f03b2a8c668cca5cc2b798480
andreashl09 <a9279352@gmail.com>
2024-06-28 17:16:15 +0200

    project is done

--- Aufgaben/Aufgaben_DAY_5/Projekt_Stein_Schere_Papier/SchereSteinPapierDuell.java
+++ Aufgaben/Aufgaben_DAY_5/Projekt_Stein_Schere_Papier/SchereSteinPapierDuell.java
@@ -23,7 +23,6 @@ public class SchereSteinPapierDuell {
                 System.err.println("Ungültige Eingbe. Bitte nochmal.");
                 continue;
             }
-
         }
     }
 
@@ -42,71 +41,26 @@ public class SchereSteinPapierDuell {
     }
 
     public static int computerRandomInt() {
-        double randomNum;
-        randomNum = Math.random() * 3;
-        int randomInt = (int) randomNum;
-        return randomInt;
+        int randomNum = (int) (Math.random() * 3);
+        return randomNum;
     }
 
     public static String stonePaperScissorSelect(int choiseInt) {
-        String theChoiseIs;
-        
-        switch (choiseInt) {
-            case 0:
-                theChoiseIs = "Schere";
-                break;
-            case 1:
-                theChoiseIs = "Stein";
-                break;
-            case 2:
-                theChoiseIs = "Papier";
-                break;
-            default:
-                theChoiseIs = "Unknow";
-                break;
-        }
-
-        return theChoiseIs;
+        String[] choiseStrings = {"Schere", "Stein", "Papier"};
+        return choiseStrings[choiseInt];
     }
 
     public static String andTheWinnerIs(int userInt, int computertInt) {
         int sum = userInt + computertInt;
-        int user = 1;
-        int computer = 2;
-        int undecided = 3;
-        int winnerIs;
-
-        // scissor = 0;
-        // stone = 1;
-        // paper = 2;
-        // sum: scissor + stone = 1, scissor + paper = 2; stone + paper = 3
-
+        String winnerText;
         if (userInt != computertInt) {
             if ((sum == 1 && userInt == 0) || (sum == 2 && userInt == 1) || (sum == 3 && userInt == 2)) {
-                winnerIs = user;
+                winnerText = "Du hast gewonnen!";
             } else {
-                winnerIs = computer;
+                winnerText = "Der Computer hat gewonnen!";
             }
         } else {
-            winnerIs = undecided;
-        }
-
-        // Gewinner Ausgabe
-        String winnerText;
-        switch (winnerIs) {
-            case 1:
-                winnerText = "Du hast gewonnen!";
-                break;
-            case 2:
-                winnerText = "Der Computer hat gewonnen!";
-                break;
-            case 3:
-                winnerText = "Unentschieden!";
-                break;
-
-            default:
-                winnerText = "Fehler";
-                break;
+            winnerText = "Unentschieden!";
         }
         return winnerText;
     }
