5c3af03ec00593e13cd2006220cd60ae531adb54
andreashl09 <a9279352@gmail.com>
2024-07-02 16:53:26 +0200

    add comments

--- Aufgaben/Aufgaben_DAY_7/Palindrom/Palindrom.java
+++ Aufgaben/Aufgaben_DAY_7/Palindrom/Palindrom.java
@@ -1,4 +1,13 @@
 public class Palindrom {
+    // Aufgabenstellung:
+    //
+    // Schreibe eine Methode, die überprüft, ob ein eingegebenes Wort oder eine Zahl
+    // ein Palindrom ist.
+
+    // Definition Palindrom: ist ein Wort, Satz, eine Zahl oder eine andere
+    // Zeichenfolge, die vorwärts und rückwärts identisch ist, wenn man die
+    // Buchstaben oder Zeichen umdreht.
+
     public static void main(String[] args) {
         String[] inputText = { "Amok Oma", "Ein Esel lese nie", "Anna", "Lagerregal" };
         for (int i = 0; i < inputText.length; i++) {
